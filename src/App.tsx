b67c11af4a3bee3f3cb648dc9e046e172c672f95
Сафиуллин Руфат Рунарович <stud0000229588@study.utmn.ru>
2023-08-23 20:47:17 +0500

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Main from './pages/Main/Main'
+import Leaderboard from './pages/Leaderboard/Leaderboard'
 
 function App() {
 	return (
 		<div className='App'>
-
+			<Routes>
+				<Route path='/' element={<Main />} />
+				<Route path='/leaderboard' element={<Leaderboard />} />
+				<Route path='*' element={<Navigate to='/' />} />
+			</Routes>
 		</div>
 	)
 }
---
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const Leaderboard: React.FC = () => {
+	return <div><h1>Leaderboard</h1></div>
+}
+
+export default Leaderboard
